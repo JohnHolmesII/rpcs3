35c99dbaecd3bb9a13669b8540334bb87e14b865
JohnHolmesII <jedistephen@hotmail.com>
2020-09-06 16:13:06 -0700

    b

--- .ci/setup-windows.sh
+++ .ci/setup-windows.sh
@@ -1,48 +1,28 @@
 #!/bin/sh -ex
 
-# These are Azure specific, so we wrap them for portability
-REPO_NAME="$BUILD_REPOSITORY_NAME"
-REPO_BRANCH="$SYSTEM_PULLREQUEST_SOURCEBRANCH"
-PR_NUMBER="$SYSTEM_PULLREQUEST_PULLREQUESTID"
+# These are CI system specific, so we wrap them for portability
+REPO_NAME="$APPVEYOR_REPO_NAME"
+REPO_BRANCH="$APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH"
+PR_NUMBER="$APPVEYOR_PULL_REQUEST_NUMBER"
 
 # Resource/dependency URLs
-# Qt mirrors can be volatile and slow, so we list 2
-QT_HOST="http://mirrors.ocf.berkeley.edu/qt/"
-#QT_HOST="http://qt.mirror.constant.com/"
-QT_URL_VER=$(echo "$QT_VER" | sed "s/\.//g")
-QT_PREFIX="online/qtsdkrepository/windows_x86/desktop/qt5_${QT_URL_VER}/qt.qt5.${QT_URL_VER}.win64_msvc2017_64/${QT_VER}-0-${QT_DATE}"
-QT_SUFFIX="-Windows-Windows_10-MSVC2017-Windows-Windows_10-X86_64.7z"
-QT_BASE_URL="${QT_HOST}${QT_PREFIX}qtbase${QT_SUFFIX}"
-QT_WINE_URL="${QT_HOST}${QT_PREFIX}qtwinextras${QT_SUFFIX}"
-QT_DECL_URL="${QT_HOST}${QT_PREFIX}qtdeclarative${QT_SUFFIX}"
-QT_TOOL_URL="${QT_HOST}${QT_PREFIX}qttools${QT_SUFFIX}"
 LLVMLIBS_URL='https://github.com/RPCS3/llvm-mirror/releases/download/custom-build-win/llvmlibs_mt.7z'
 #GLSLANG_URL='https://github.com/RPCS3/glslang/releases/download/custom-build-win/glslanglibs_mt.7z'  <- Temporarily disabled auto-builds
 GLSLANG_URL='https://www.dropbox.com/s/cg48qr4zmnn066v/glslanglibs_mt.7z'
 VULKAN_SDK_URL="https://www.dropbox.com/s/adanclixregbp2x/VulkanSDK-${VULKAN_VER}-Installer.exe"
 
-DEP_URLS="         \
-    $QT_BASE_URL   \
-    $QT_WINE_URL   \
-    $QT_DECL_URL   \
-    $QT_TOOL_URL   \
+DEP_URLS="
     $LLVMLIBS_URL  \
     $GLSLANG_URL   \
     $VULKAN_SDK_URL"
 
-# Azure pipelines doesn't make a cache dir if it doesn't exist, so we do it manually
-[ -d "$CACHE_DIR" ] || mkdir "$CACHE_DIR"
+# Appveyor doesn't make a cache dir if it doesn't exist, so we do it manually
+mkdir -p "$CACHE_DIR"
 
 # Pull all the submodules except llvm, since it is built separately and we just download that build
 # Note: Tried to use git submodule status, but it takes over 20 seconds
 # shellcheck disable=SC2046
 git submodule -q update --init --depth 1 $(awk '/path/ && !/llvm/ { print $3 }' .gitmodules)
-
-# Git bash doesn't have rev, so here it is
-rev()
-{
-    echo "$1" | awk '{ for(i = length($0); i != 0; --i) { a = a substr($0, i, 1); } } END { print a }'
-}
 
 # Usage: download_and_verify url checksum algo file
 # Check to see if a file is already cached, and the checksum matches. If not, download it.
@@ -64,12 +44,12 @@ download_and_verify()
     return 1;
 }
 
-# Some dependencies install here
+# Prebuilt libs install here
 [ -d "./lib" ] || mkdir "./lib"
 
 for url in $DEP_URLS; do
     # Get the filename from the URL. Breaks if urls have js args, so don't do that pls
-    fileName="$(rev "$(rev "$url" | cut -d'/' -f1)")"
+    fileName="$(basename "$url")"
     [ -z "$fileName" ] && echo "Unable to parse url: $url" && exit 1
 
     # shellcheck disable=SC1003
